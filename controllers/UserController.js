c95ee4b50ea849f9a3a7fd898429f345a4e549c0
Gabriel Rodrigues <contatogabrielcruzrodrigues@gmail.com>
2023-07-17 08:59:46 -0300

    evitando cadastro de email duplicados

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -4,12 +4,23 @@ const CategoryModel = require("../model/CategoryModel");
 const bcrypt = require("bcryptjs");
 
 exports.findAll = (req, res) => {
-
+    UserModel.findAll()
+        .then((users) => {
+            CategoryModel.findAll()
+                .then((categories) => {
+                    res.render('admin/users/users', { users: users, categories: categories });
+                }).catch((error) => {
+                    res.redirect('/');
+                });
+        })
+        .catch((error) => {
+            res.redirect('/');
+        })
 }
 
 exports.pageCreate = (req, res) => {
     CategoryModel.findAll().then((categories) => {
-        res.render('admin/users/create', {categories: categories});   
+        res.render('admin/users/create', { categories: categories });
     })
 }
 
@@ -17,15 +28,23 @@ exports.create = (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(password, salt);
+    UserModel.findOne({ where: { email: email } }).then((verificationEmail) => {
+        if (verificationEmail == undefined) {
+            const salt = bcrypt.genSaltSync(10);
+            const hash = bcrypt.hashSync(password, salt);
 
-    UserModel.create({
-        email: email,
-        password: hash
-    }).then(() => {
-        res.redirect('/');
-    }).catch((error) => {
-        res.redirect('/');
+            UserModel.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect('/');
+            }).catch((error) => {
+                res.redirect('/');
+            })
+        } else {
+            res.redirect("/admin/users/create");
+        }
     })
+
+
 }
